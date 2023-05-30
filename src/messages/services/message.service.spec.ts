f9eb49450ea543b3554d7faf5a85029b713e7625
SebastianAbril6 <sebastianabril6@gmail.com>
2023-05-30 11:53:41 -0500

    more unit testing

--- src/messages/services/message.service.spec.ts
+++ src/messages/services/message.service.spec.ts
@@ -1,4 +1,5 @@
 import { User } from '../../auth/entity/user.entity';
+import { Message } from '../entity/message.entity';
 import { MessageService } from './message.service';
 
 describe('MessageService', () => {
@@ -31,8 +32,8 @@ describe('MessageService', () => {
       const userId = 1;
       const title = 'Este es el mensaje';
       const content = 'bla bla bla, y entonces bla';
-
       userRepository.findOneBy.mockResolvedValueOnce(null);
+
       await expect(async () => {
         await messageService.createMessage(userId, title, content);
       }).rejects.toThrow(`User with id ${userId} not found`);
@@ -44,7 +45,6 @@ describe('MessageService', () => {
       const content = 'bla bla bla, y entonces bla';
       const user = new User();
       user.id = userId;
-
       userRepository.findOneBy.mockResolvedValueOnce(user);
 
       await messageService.createMessage(userId, title, content);
@@ -84,6 +84,90 @@ describe('MessageService', () => {
 
       await messageService.getMessagesByUser(userId);
       expect(messageRepository.findMessagesByUser).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('Should get a message by its id', () => {
+    test('throw NotFound Exception when messsage id does not exist', async () => {
+      const messageId = 10;
+      messageRepository.findMessageById.mockResolvedValueOnce(null);
+
+      await expect(async () => {
+        await messageService.getMessageById(messageId);
+      }).rejects.toThrow(`Message with id ${messageId} was not found`);
+    });
+
+    test('should get a message by its id sucessfully', async () => {
+      const messageId = 10;
+      const message = new Message();
+      message.id = messageId;
+      messageRepository.findMessageById.mockResolvedValueOnce(message);
+
+      await messageService.getMessageById(messageId);
+      expect(messageRepository.findMessageById).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('Should delete a message by its id', () => {
+    test('Throw NotFound Exception when user id does not exist', async () => {
+      const userId = 7;
+      const messageId = 4;
+      userRepository.findOneBy.mockResolvedValueOnce(null);
+
+      await expect(async () => {
+        await messageService.deleteMessageById(messageId, userId);
+      }).rejects.toThrow(`The user with id ${userId} was not found`);
+    });
+
+    test('Throw NotFound Exception when message id does not exist', async () => {
+      const userId = 7;
+      const messageId = 4;
+      const user = new User();
+      user.id = userId;
+      userRepository.findOneBy.mockResolvedValueOnce(user);
+      messageRepository.findMessageById.mockResolvedValueOnce(null);
+
+      await expect(async () => {
+        await messageService.deleteMessageById(messageId, user.id);
+      }).rejects.toThrow(
+        `Message with id ${messageId} could not be deleted, it was not found`,
+      );
+    });
+
+    test("Throw BadRequest Exception when a user tries to delete other user's comments", async () => {
+      const userId = 7;
+      const messageId = 4;
+      const user = new User();
+      user.id = userId;
+      const message = new Message();
+      message.id = messageId;
+      message.userId = 44;
+
+      userRepository.findOneBy.mockResolvedValueOnce(user);
+      messageRepository.findMessageById.mockResolvedValueOnce(message);
+
+      await expect(async () => {
+        await messageService.deleteMessageById(message.id, user.id);
+      }).rejects.toThrow(`You can not delete other person's comments`);
+    });
+
+    test('Should delete a message sucessfully', async () => {
+      const userId = 7;
+      const messageId = 17;
+      const user = new User();
+      const message = new Message();
+
+      user.id = userId;
+      message.id = messageId;
+      message.userId = userId;
+      message.title = 'Buenos dias';
+      message.text = 'Mensaje se acaba de borrar';
+
+      userRepository.findOneBy.mockResolvedValueOnce(user);
+      messageRepository.findMessageById.mockResolvedValueOnce(message);
+
+      await messageService.deleteMessageById(message.id, user.id);
+      expect(messageRepository.remove).toHaveBeenCalledTimes(1);
     });
   });
 });
